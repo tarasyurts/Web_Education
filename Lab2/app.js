f00d0eb2c69306696f0f32865d9c901245f4418e
Taras <tariksyurts@gmail.com>
2020-12-18 05:45:07 +0200

    lab2 done

---
+++ Lab2/app.js
@@ -0,0 +1,26 @@
+var calcBtn = document.querySelector("#calcBtn");
+
+calcBtn.addEventListener("click", function () {
+  let sideFirst = document.getElementById("sideFirst").value;
+  let sideSecond = document.getElementById("sideSecond").value;
+  let resultTag = document.querySelector("#result");
+
+  if(sideFirst==0 || sideSecond==0){
+    resultTag.innerHTML = 'Сторона не може бути рівна 0';
+    return;
+  }
+
+  if(sideFirst<0 || sideSecond<0){
+    resultTag.innerHTML = 'Сторона не може бути менша 0';
+    return;
+  }
+
+  if(sideFirst>sideSecond){
+      [sideFirst, sideSecond] = [sideSecond, sideFirst];
+  }
+
+  
+  let result = Math.sqrt(Math.pow(parseInt(sideSecond), 2) - Math.pow(parseInt(sideFirst), 2)).toFixed(2);
+
+  resultTag.innerHTML = 'Результат= ' + result;
+});
